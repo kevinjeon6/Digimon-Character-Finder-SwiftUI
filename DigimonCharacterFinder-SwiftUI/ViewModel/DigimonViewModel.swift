2e2857d4641908783b979309fb6392f887c294be
Kevin <kevin_jeon@icloud.com>
2023-06-19 16:09:16 -0400

    Create view model to fetch digimon data

---
+++ DigimonCharacterFinder-SwiftUI/ViewModel/DigimonViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  DigimonViewModel.swift
+//  DigimonCharacterFinder-SwiftUI
+//
+//  Created by Kevin Mattocks on 6/19/23.
+//
+
+import Foundation
+
+@MainActor
+class DigimonViewModel: ObservableObject {
+    
+    @Published var characters = [Digimon]()
+    
+    
+    func getDigimonData() async throws {
+        
+        guard let url = URL(string: "https://digimon-api.vercel.app/api/digimon") else { return print("This is a bad URL")}
+        
+        let urlRequest = URLRequest(url: url)
+        
+        do {
+            
+            let (data, response) = try await URLSession.shared.data(for: urlRequest)
+            
+            guard(response as? HTTPURLResponse)?.statusCode == 200 else { return print("Server error")}
+            
+            let decoder = JSONDecoder()
+            
+            self.characters = try decoder.decode([Digimon].self, from: data)
+            
+            
+        } catch {
+            print("Error: \(error.localizedDescription)")
+        }
+        
+    }
+    
+}

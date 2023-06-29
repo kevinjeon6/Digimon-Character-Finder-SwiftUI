9b040c6329d4a40f112d3af0431ca5140eddac68
Kevin <kevin_jeon@icloud.com>
2023-06-29 14:32:40 -0400

    Add Digimon entity with attributes Add DataController for Core data to handle logic Create  a tab bar to display digmon characters and favorites

---
+++ DigimonCharacterFinder-SwiftUI/CoreData/DataController.swift
@@ -0,0 +1,106 @@
+//
+//  DataController.swift
+//  DigimonCharacterFinder-SwiftUI
+//
+//  Created by Kevin Mattocks on 6/28/23.
+//
+
+import CoreData
+import Foundation
+import SwiftUI
+
+
+//MVVM with Core Data
+
+class DataController: ObservableObject {
+    
+    let container = NSPersistentContainer(name: "DigimonCharacterFinder_SwiftUI")
+    private let entityName = "DigimonEntity"
+    
+    @Published var savedEntities = [DigimonEntity]()
+    
+    
+    init() {
+        //Load persistent stores when created
+        container.loadPersistentStores { description, error in
+            if let error {
+                print("Core data failed to load \(error.localizedDescription)")
+            } else {
+                print("Core data successfully loaded")
+            }
+        }
+    }
+    
+    // MARK: - Methods
+    func getFavorites() {
+         //Create the fetch request
+         //Need to give a specific result type which is the type we are trying to fetch in this case is DigimonEntity
+         //The request is getting all entities in the container
+         let request = NSFetchRequest<DigimonEntity>(entityName: entityName)
+         
+         do {
+             //try and fetch from the container and result will be stored and saved in the savedEntities
+             savedEntities = try container.viewContext.fetch(request)
+         } catch {
+             print("Error fetching Digimon Entities")
+         }
+     }
+    
+    //Pass in Digimon to function and convert it to an entity
+    func addItem(character: Digimon) {
+        //When adding, create the entity and provide it some data and then save
+        //In this case when getting data from JSON, we Convert Digimon model to entity we want to save
+        let entity = DigimonEntity(context: container.viewContext)//container refers to the container we made container = NSPersistentContainer(name: "")
+        entity.name = character.name
+        entity.level = character.level
+        entity.img = character.img
+        saveData()
+    }
+    
+    func saveData() {
+        do {
+            try container.viewContext.save()
+            getFavorites() //refetchign from the context
+        } catch {
+            print("Error saving the data to Core Data")
+        }
+    }
+    
+    func deleteItem(offsets: IndexSet) {
+       //Delete objects here
+        //[$0] the first index
+       offsets.map { savedEntities[$0] }.forEach(container.viewContext.delete)
+       saveData()
+   }
+    
+    func deleteFavorite(character: Digimon) throws {
+        //
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+
+        let namePredicate = NSPredicate(format: "name == %@", character.name)
+        let levelPredicate = NSPredicate(format: "level == %@", character.level)
+        let imgPredicate = NSPredicate(format: "img == %@", character.img)
+
+        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [namePredicate, levelPredicate, imgPredicate])
+        request.predicate = compoundPredicate
+
+
+        do {
+            let deleteResult = try container.viewContext.execute(NSBatchDeleteRequest(fetchRequest: request))
+            saveData()
+
+        } catch {
+            //Handle errors
+            print(error)
+            print("There was an error deleting favorite")
+        }
+    }
+    
+    func isFavorite(character: Digimon) -> Bool {
+        savedEntities.contains {
+            $0.name == character.name && $0.img == character.img && $0.level == character.level
+        }
+    }
+
+    
+}
---
+++ DigimonCharacterFinder-SwiftUI/Views/TabBarView.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarView.swift
+//  DigimonCharacterFinder-SwiftUI
+//
+//  Created by Kevin Mattocks on 6/28/23.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    
+    @State private var selectedTab = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            
+            DigimonListView()
+                .tabItem {
+                    Label("Digmon", systemImage: "list.dash")
+                }
+                .tag(0)
+            
+            FavoritesView()
+                .tabItem {
+                    Label("Favorites", systemImage: "star.fill")
+                }
+                .tag(1)
+            
+        }
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
